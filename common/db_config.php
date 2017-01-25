0d48cf479763025d4d60a463002d046291df8234
leejaebang <now0569@gmail.com>
2017-01-25 17:44:22 +0900

    제거됨

--- common/db_config.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('DB_SERVER', 'localhost');
-define('DB_USER', 'ninfolist');
-define('DB_PASSWORD', 'thskrl!@rmfls');
-define('DB_NAME', 'ninfolist');
-?>
--- common/db_connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-include 'db_config.php';
-
-$conn = @mysql_connect(DB_SERVER,DB_USER,DB_PASSWORD) or die("Connection Error: " . @mysql_error());
-@mysql_select_db(DB_NAME) or die("Error connecting to db.");
-//echo $conn;
-?>
